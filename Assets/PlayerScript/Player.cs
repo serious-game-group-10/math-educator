355e60a0a5edbc47da58260a236f95285716e0da
Ruimin Xu <ruiminxu@Ruimins-MacBook-Air.local>
2021-10-22 17:52:28 -0400

    pushing

---
+++ Assets/PlayerScript/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D player;
+    Vector2 movement;
+    private float speed = 10f;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+    }
+
+    private void FixedUpdate()
+    {
+        player.velocity = speed * movement;
+    }
+}
