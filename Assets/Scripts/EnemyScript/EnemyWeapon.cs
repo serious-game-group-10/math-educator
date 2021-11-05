9c1885264188a0bfed921de2a50784ad8e5a968e
Ruimin Xu <ruiminxu@Ruimins-Air.fios-router.home>
2021-11-05 13:08:05 -0400

    added attack script for player and enemy

---
+++ Assets/Scripts/EnemyScript/EnemyWeapon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyWeapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
