9842bbe0929c4b0ff220f06eb62531c936347f9c
Ruimin Xu <ruiminxu@Ruimins-Air.fios-router.home>
2021-12-18 14:53:17 -0500

    added highscore script

--- Assets/Scripts/Persisting Data/DataPersistor.cs
+++ Assets/Scripts/Persisting Data/DataPersistor.cs
@@ -6,6 +6,9 @@ public class DataPersistor : MonoBehaviour
 {
     private float gameVolume;
     private bool inFight;
+    private int currentScore;
+    private string playerName;
+    private Highscore[] highscoreDB;
 
     public static DataPersistor instance = null;
 
@@ -25,6 +28,9 @@ public class DataPersistor : MonoBehaviour
     {
         gameVolume = 0.5f;
         inFight = false;
+        currentScore = 0;
+        playerName = "";
+        highscoreDB = new Highscore[5];
     }
 
     public void setGameVolume(float gameVolume)
@@ -37,6 +43,31 @@ public class DataPersistor : MonoBehaviour
         this.inFight = inFight;
     }
 
+    public void setPlayername(string playerName)
+    {
+        this.playerName = playerName;
+    }
+
+    public void addScore()
+    {
+        this.currentScore = this.currentScore + 1;
+    }
+
+    public int getCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public Highscore[] getHighscoreDB()
+    {
+        return highscoreDB;
+    }
+
+    public string getPlayerName()
+    {
+        return playerName;
+    }
+
     public bool getInFight()
     {
         return inFight;
@@ -45,5 +76,37 @@ public class DataPersistor : MonoBehaviour
     public float getGameVolume()
     {
         return gameVolume;
+    }
+
+    public void gameReset()
+    {
+        this.playerName = "";
+        this.currentScore = 0;
+    }
+
+    public void addHighScore()
+    {
+        for (int i = 0; i < highscoreDB.Length; i++)
+        {
+            if (highscoreDB[i] == null)
+            {
+                highscoreDB[i] = new Highscore(playerName, currentScore);
+                return;
+            }
+        }
+        bubbleSort(highscoreDB);
+    }
+
+    private void bubbleSort(Highscore[] highscore)
+    {
+        int n = highscore.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (highscore[j].getCurrentScore() < highscore[j + 1].getCurrentScore())
+                {
+                    Highscore temp = highscore[j];
+                    highscore[j] = highscore[j + 1];
+                    highscore[j + 1] = temp;
+                }
     }
 }
